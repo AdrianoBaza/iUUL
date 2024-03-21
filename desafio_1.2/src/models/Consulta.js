b994ad9ae2fcc0087f42ae593f21b12e2538f2cf
AdrianoBaza <bazafordev@gmail.com>
2024-03-20 22:08:42 -0300

    feat: add Consulta.js

---
+++ desafio_1.2/src/models/Consulta.js
@@ -0,0 +1,8 @@
+export class Consulta {
+  constructor (paciente, data, horaInicio, horaFim) {
+    this.paciente = paciente
+    this.data = data
+    this.horaInicio = horaInicio
+    this.horaFim = horaFim
+  }
+}

d5b58de820bfcec4f0add564184acc0d6f4a0ae0
AdrianoBaza <bazafordev@gmail.com>
2024-04-03 21:51:43 -0300

    feat: add errosPresenter.js

---
+++ desafio_2.1/presenter/errosPresenter.js
@@ -0,0 +1,32 @@
+const _ = require('lodash');
+
+class ErrosPresenter {
+  constructor(erros) {
+    this.erros = erros;
+  }
+
+  getErros() {
+    return {
+      erros: this.erros.map((erro) => ({
+        dados: erro.dados,
+        erros: erro.erros.map((e) => ({
+          campo: e.campo,
+          mensagem: e.mensagem,
+        })),
+      })),
+    };
+  }
+
+  getErrosFormatados() {
+    const errosFormatados = [];
+    for (const erro of this.erros) {
+      errosFormatados.push({
+        ...erro.dados,
+        erros: _.map(erro.erros, 'mensagem').join(', '),
+      });
+    }
+    return errosFormatados;
+  }
+}
+
+module.exports = ErrosPresenter;
